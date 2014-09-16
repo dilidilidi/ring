538ed084828a5a04876a2a87151384c3ac81fab9
dilidilidi <93146139@qq.com>
2014-09-16 15:03:57 +0800

    change to gulp

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    header = require('gulp-header'),
+
+    pkg = require('./package.json'),
+    currentYear = util.date(new Date(), 'yyyy');
+
+var paths = {
+    scripts: [
+        './src/ring.js'
+    ]
+};
+
+var banner = '/*! <%= pkg.name %> v<%= pkg.version %> | Copyright (c) 2014-<%= currentYear %> <%= pkg.author %> | <%= pkg.license %> license | <%= pkg.homepage %> */\n';
+
+gulp.task('default', function () {
+    return gulp.src(paths.scripts)
+        .pipe(uglify())
+        .pipe(header(banner, {pkg: pkg, currentYear: currentYear}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'))
+});
